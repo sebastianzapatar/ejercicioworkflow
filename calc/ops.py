68be363ebb0fff18b8c49ec655a333772c87986b
Sebastian Zapata <pansezapata@gmail.com>
2025-08-22 14:48:49 -0500

    Se crea el workflow con python y pytest

---
+++ calc/ops.py
@@ -0,0 +1,6 @@
+from typing import Union
+
+Number = Union[int, float]
+
+def add(a: Number, b: Number) -> Number:
+    return a + b
---
+++ tests/test_ops.py
@@ -0,0 +1,11 @@
+import pytest
+
+from calc.ops import add
+
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    (-5, 5, 0),
+    (0, 0, 0)
+])
+def test_add(a, b, expected):
+    assert add(a, b) == expected
