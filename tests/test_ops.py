cf1f587060dd596ebfff959a88a9582ba45765cb
Sebastian Zapata <pansezapata@gmail.com>
2025-08-22 14:53:15 -0500

    Pruebas ok

--- tests/test_ops.py
+++ tests/test_ops.py
@@ -5,7 +5,7 @@ from calc.ops import add
 @pytest.mark.parametrize("a, b, expected", [
     (1, 2, 3),
     (-5, 5, 0),
-    (0, 0, 2)
+    (0, 0, 0)
 ])
 def test_add(a, b, expected):
     assert add(a, b) == expected
